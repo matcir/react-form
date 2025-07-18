ac466603c65b40911d4f5c56b68d7ce3b4c13e8d
matcir <matteocirilloboolean@gmail.com>
2025-07-18 12:56:38 +0200

    add form with new article input with page updating upon adding new item

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function App() {
 
-  const articles = [
+  const initial_articles = [
     {
       id: 1,
       name: "apple",
@@ -30,16 +30,40 @@ function App() {
     }
   ]
 
+  // const initial_articles = [
+  //   "apple",
+  //   "bread",
+  //   "water",
+  //   "sausage",
+  //   "cucumber"
+  // ]
+
+  const [newArticle, setNewArticle] = useState("")
+  const [articles, setArticles] = useState(initial_articles)
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setArticles([...articles, {id:(articles.length+1), name:newArticle, description:""}])
+  }
+
+
   return (
     <>
       <div className='container'>
           <h1>Articoli</h1>
+
           <ul className='mt-3 list-group'>
             {articles.map((article) => (
               <li key={article.id} className='list-group-item'>{article.name}</li>
             ))}
-
           </ul>
+
+          <form onSubmit={handleSubmit}>
+            <div className='d-flex'>
+              <input type="text" className='form-control mt-2' placeholder='inserisci un nuovo articolo' value={newArticle} onChange={e => setNewArticle(e.target.value)}/>
+              <button type='submit' className='btn btn-primary'>Save</button>
+            </div>
+          </form>
       </div>
     </>
   )
