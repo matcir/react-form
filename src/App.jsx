3d3ffad5f5c315a06f4725fe097788ba85673386
matcir <matteocirilloboolean@gmail.com>
2025-07-18 17:18:50 +0200

    add edit function

--- src/App.jsx
+++ src/App.jsx
@@ -30,16 +30,10 @@ function App() {
     }
   ]
 
-  // const initial_articles = [
-  //   "apple",
-  //   "bread",
-  //   "water",
-  //   "sausage",
-  //   "cucumber"
-  // ]
-
   const [newArticle, setNewArticle] = useState("")
   const [articles, setArticles] = useState(initial_articles)
+  const [editingIndex, setEditingIndex] = useState(null)
+  const [editedName, setEditedName] = useState("")
 
   function handleSubmit(e){
     e.preventDefault()
@@ -52,6 +46,21 @@ function App() {
     
   }
 
+  function handleEditClick(index){
+    setEditingIndex(index)
+    setEditedName(articles[index].name)
+  }
+
+  function handleSaveClick(index) {
+
+    const updatedArticles = [...articles];
+    updatedArticles[index] = {
+      ...updatedArticles[index],
+      name: editedName
+    }
+    setArticles(updatedArticles)
+    setEditingIndex(null); 
+}
 
   return (
     <>
@@ -60,14 +69,31 @@ function App() {
 
           <ul className='mt-3 list-group'>
             {articles.map((article,index) => (
-              <li key={index} className='d-flex justify-content-between list-group-item'>{article.name} <button className='btn'><i className='bi bi-trash'onClick={() => handleDeleteClick(index)}></i></button> </li>
+              <li key={index} className='d-flex justify-content-between list-group-item'>{editingIndex === index ? 
+              (
+                 <>
+                  <input type="text" className="form-control" value={editedName} onChange={(e) => setEditedName(e.target.value)}/>
+                  <button className='btn btn-success' onClick={() => handleSaveClick(index)}>Salva</button>
+                  <button className='btn btn-secondar' onClick={() => setEditingIndex(null)}>Annulla</button>
+                </>
+
+              ) : (
+                <>
+                {article.name} 
+                <div>
+                  <button className='btn'><i className='bi bi-pencil'onClick={() => handleEditClick(index)}></i></button> 
+                  <button className='btn'><i className='bi bi-trash'onClick={() => handleDeleteClick(index)}></i></button> 
+                </div>
+                </>
+              )}
+            </li>
             ))}
           </ul>
 
           <form onSubmit={handleSubmit}>
-            <div className='d-flex'>
-              <input type="text" className='form-control mt-2' placeholder='inserisci un nuovo articolo' value={newArticle} onChange={e => setNewArticle(e.target.value)}/>
-              <button type='submit' className='btn btn-primary'>Save</button>
+            <div className='d-flex mt-2'>
+              <input type="text" className='form-control' placeholder='inserisci un nuovo articolo' value={newArticle} onChange={e => setNewArticle(e.target.value)}/>
+              <button type='submit' className='btn btn-primary '>Save</button>
             </div>
           </form>
       </div>
